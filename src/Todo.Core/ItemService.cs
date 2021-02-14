5fbcc7775d0893778504230cfb83ccab5b9297bb
Anttijuhani Lantto <88804+alantto@users.noreply.github.com>
2021-02-14 21:48:27 +0200

    Move ItemService to correct folder

--- src/Todo.Core/ItemService.cs
+++ src/Todo.Core/ItemService.cs
@@ -1,9 +1,9 @@
 using System;
 using System.Collections.Generic;
-using System.Dynamic;
 using System.Linq;
+using Todo.Core.Models;
 
-namespace Todo.Core.Models
+namespace Todo.Core
 {
     public class ItemService
     {
