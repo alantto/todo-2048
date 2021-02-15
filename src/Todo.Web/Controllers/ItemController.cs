bdf3e826e85b579abcdd309fb9adce8ac35f1fc9
Anttijuhani Lantto <88804+alantto@users.noreply.github.com>
2021-02-15 09:11:04 +0200

    Fix ItemService reference

--- src/Todo.Web/Controllers/ItemController.cs
+++ src/Todo.Web/Controllers/ItemController.cs
@@ -1,9 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using Todo.Core;
 using Todo.Core.Models;
 
 namespace Todo.Web.Controllers
